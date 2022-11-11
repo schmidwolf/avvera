8f6fb95ed378a310b32faf027eaaa74fb6e0f93d
schmidwolf <info@schmid-wolf.ch>
2022-11-11 08:34:47 +0100

    1.0

---
+++ textunited_switch_image.js
@@ -0,0 +1,20 @@
+addEventListener('DOMContentLoaded', () => {
+    if (document.cookie.includes("TextUnitedSelectedLanguage=40")) {
+        $('.is-german').css('display', 'none');
+        $('.is-english').css('display', 'block');
+    } else {
+        $('.is-english').css('display', 'none');
+        $('.is-german').css('display', 'block');
+    }
+});
+
+
+$('*[data-id="40"]').on('click', function() {
+    $('.is-german').css('display', 'none');
+    $('.is-english').css('display', 'block');
+  });
+
+$('*[data-id="57"]').on('click', function() {
+    $('.is-english').css('display', 'none');
+    $('.is-german').css('display', 'block');
+  });
