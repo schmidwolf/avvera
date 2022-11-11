57b0953080eb1494ed2324d57591dfa3ffb54382
schmidwolf <info@schmid-wolf.ch>
2022-11-11 09:07:25 +0100

    1.2

--- textunited_switch_image.js
+++ textunited_switch_image.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', (event) => {
     if (document.cookie.includes("TextUnitedSelectedLanguage=40")) {
         $('.is-german').css('display', 'none');
-        $('.is-english').css('display', 'block');
+        $('.is-english').css('display', 'inline-block');
         alert ("Englisch");
     } else {
         $('.is-english').css('display', 'none');
-        $('.is-german').css('display', 'block');
+        $('.is-german').css('display', 'inline-block');
         alert ("Deutsch");
     }
 });
@@ -13,12 +13,12 @@ window.addEventListener('load', (event) => {
 
 $('[data-id="40"]').on('click', function() {
     $('.is-german').css('display', 'none');
-    $('.is-english').css('display', 'block');
+    $('.is-english').css('display', 'inline-block');
     alert ("Englisch");
 });
 
 $('[data-id="57"]').on('click', function() {
     $('.is-english').css('display', 'none');
-    $('.is-german').css('display', 'block');
+    $('.is-german').css('display', 'inline-block');
     alert ("Deutsch");
 });
