16d79dafea2f9d391db96dae6a0769ffcae2b1ad
schmidwolf <info@schmid-wolf.ch>
2022-11-11 11:24:45 +0100

    Switch to jQuery

--- textunited_switch_image.js
+++ textunited_switch_image.js
@@ -1,27 +1,24 @@
-window.addEventListener('load', (event) => {
+$( document ).ready(function() {
     if (document.cookie.includes("TextUnitedSelectedLanguage=40")) {
-        document.querySelector(".is-german").style.display = "none";
-        document.querySelector(".is-english").style.display = "inline-block";
+        $('.is-german').css('display', 'none');
+        $('.is-english').css('display', 'inline-block');
         alert ("Englisch");
     } else {
-        document.querySelector(".is-german").style.display = "inline-block";
-        document.querySelector(".is-english").style.display = "none";
+        $('.is-english').css('display', 'none');
+        $('.is-german').css('display', 'inline-block');
         alert ("Deutsch");
     }
 });
 
 
-document.querySelector('[data-id="40"]').addEventListener("click", isEnglish); 
-document.querySelector('[data-id="57"]').addEventListener("click", isGerman); 
-
-function isEnglish(){
-    document.querySelector(".is-german").style.display = "none";
-    document.querySelector(".is-english").style.display = "inline-block";
+$('[data-id="40"]').on('click', function() {
+    $('.is-german').css('display', 'none');
+    $('.is-english').css('display', 'inline-block');
     alert ("Englisch");
-};
+});
 
-function isGerman(){
-    document.querySelector(".is-german").style.display = "inline-block";
-    document.querySelector(".is-english").style.display = "none";
+$('[data-id="57"]').on('click', function() {
+    $('.is-english').css('display', 'none');
+    $('.is-german').css('display', 'inline-block');
     alert ("Deutsch");
-};
+});
