d54c339a0b963ab7e059a0c3293a3c8d2819dd2d
schmidwolf <info@schmid-wolf.ch>
2022-11-11 09:01:57 +0100

    Update textunited_switch_image.js

--- textunited_switch_image.js
+++ textunited_switch_image.js
@@ -2,9 +2,11 @@ window.addEventListener('load', (event) => {
     if (document.cookie.includes("TextUnitedSelectedLanguage=40")) {
         $('.is-german').css('display', 'none');
         $('.is-english').css('display', 'block');
+        alert ("Englisch");
     } else {
         $('.is-english').css('display', 'none');
         $('.is-german').css('display', 'block');
+        alert ("Deutsch");
     }
 });
 
